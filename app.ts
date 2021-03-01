84a4df4b1da926f3613df175b19e45b6a5b92407
carr0495@algonquinlive.com <carr0495@algonquinlive.com>
2021-03-01 16:08:20 -0500

    testing out typescript using new ES functionality and auto-compiling ES5

--- app.ts
+++ app.ts
@@ -1,2 +1,11 @@
-let todo: string = "Pick up drycleaning";
-let name: string = "Jess";
+var container = document.getElementById("container");
+
+function countdown(initial, final = 0, interval = 1) {
+  let current = initial;
+  while (current > final) {
+    container.innerHTML = current;
+    current -= interval;
+  }
+}
+
+countdown(10, 4, 2);
