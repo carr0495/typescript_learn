bb2ac973ad7340f3e79b6b56377b47717e0022c5
carr0495@algonquinlive.com <carr0495@algonquinlive.com>
2021-03-01 16:21:36 -0500

    practicing template string in typescript

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var container = document.getElementById("container");
-function countdown(initial, final, interval) {
-    if (final === void 0) { final = 0; }
-    if (interval === void 0) { interval = 1; }
-    var current = initial;
-    while (current > final) {
-        container.innerHTML = current;
-        current -= interval;
-    }
-}
-countdown(10, 4, 2);
+//template strings
+var todo = {
+    id: 123,
+    name: "pick up drycleaning",
+    completed: true,
+};
+container.innerHTML = "<div todo=" + todo.id + " class=\"list-group-item}\">\n<i class=\"" + (todo.completed ? "" : "hidden") + " text-success glyphicon glyphicon-ok\"></i>\n<span class=\"name\">" + todo.name + "</span>\n</div>";
+var displayName = "Todo#" + todo.id;
+// lesson 2 - Default Parameters
+// function countdown(initial, final = 0, interval = 1) {
+//   let current = initial;
+//   while (current > final) {
+//     container.innerHTML = current;
+//     current -= interval;
+//   }
+// }
+// countdown(10, 4, 2);
--- app.ts
+++ app.ts
@@ -1,11 +1,28 @@
 var container = document.getElementById("container");
 
-function countdown(initial, final = 0, interval = 1) {
-  let current = initial;
-  while (current > final) {
-    container.innerHTML = current;
-    current -= interval;
-  }
-}
+//template strings
+let todo = {
+  id: 123,
+  name: "pick up drycleaning",
+  completed: true,
+};
 
-countdown(10, 4, 2);
+container.innerHTML = `<div todo=${todo.id} class="list-group-item}">
+<i class="${
+  todo.completed ? "" : "hidden"
+} text-success glyphicon glyphicon-ok"></i>
+<span class="name">${todo.name}</span>
+</div>`;
+
+let displayName = `Todo#${todo.id}`;
+
+// lesson 2 - Default Parameters
+// function countdown(initial, final = 0, interval = 1) {
+//   let current = initial;
+//   while (current > final) {
+//     container.innerHTML = current;
+//     current -= interval;
+//   }
+// }
+
+// countdown(10, 4, 2);
